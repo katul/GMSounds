75c23c72d60d4be760c47b301247132897849c7e
Atul Kumar <akguptah@gmail.com>
2020-03-16 13:02:58 +0530

    Added logs to php

--- forms/contact.php
+++ forms/contact.php
@@ -1,9 +1,10 @@
 <?php
+	console.log("Called file contact.php");
    $to_email = "akguptah@gmail.com";
    $subject = "Simple Email Test via PHP";
    $body = "Hi,nn This is test email send by PHP Script";
    $headers = "From: akguptah@gmail.com";
- 
+	console.log("Before mail method");
    if ( mail($to_email, $subject, $body, $headers)) {
       echo("Email successfully sent to $to_email...");
    } else {
