b237bd2709694bca7de9cda17bdcdc81795e093d
Atul Kumar <akguptah@gmail.com>
2020-04-19 01:05:21 -0500

    fix instalink

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,7 +87,7 @@
     if ($(this).scrollTop() > 100) {
       $('#header').addClass('header-scrolled');
     } else {
-      $('#header').removeClass('header-scrolled');
+      $('#header').addClass('header-scrolled');
     }
   });
 
